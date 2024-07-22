b96b03f33050875bdb9fe30046e7a5762508e140
Thomaslund1 <159867737+Thomaslund1@users.noreply.github.com>
2024-07-22 14:47:38 -0500

    Create graphByIndex.py

---
+++ examples/graphByIndex.py
@@ -0,0 +1,17 @@
+import lib
+import numpy as np
+import matplotlib.pyplot as plt
+
+#grab data
+wavl = hpy.File("/data1/linefits/outputsEtalonNEID/consolidatedData_current/All_centroidWl.hdf5",'r')['dat'][:]
+ords = hpy.File("/data1/linefits/outputsEtalonNEID/consolidatedData_current/All_order.hdf5",'r')['dat'][:]
+
+#process data by index
+ordVel = getVels(wavl,ords,15,1)
+
+#graph a few bins
+fig = plt.figure()
+for i in range(5):
+    plt.plot(boxcar_median(arr[1500+(i*10)],100),label=i)
+plt.xlabel("measurment number")
+plt.ylabel("ervs")
