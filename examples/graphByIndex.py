b33d952185ca04963c451a531b609f5faf8b4a3c
Fos314 <164817701+Fos314@users.noreply.github.com>
2024-07-23 09:39:22 -0500

    Update graphByIndex.py

--- examples/graphByIndex.py
+++ examples/graphByIndex.py
@@ -7,11 +7,11 @@ wavl = hpy.File("/data1/linefits/outputsEtalonNEID/consolidatedData_current/All_
 ords = hpy.File("/data1/linefits/outputsEtalonNEID/consolidatedData_current/All_order.hdf5",'r')['dat'][:]
 
 #process data by index
-ordVel = getVels(wavl,ords,15,1)
+ordVel = lib.getVels(wavl,ords,15,1)
 
 #graph a few bins
 fig = plt.figure()
 for i in range(5):
-    plt.plot(boxcar_median(arr[1500+(i*10)],100),label=i)
+    plt.plot(lib.boxcar_median(arr[1500+(i*10)],100),label=i)
 plt.xlabel("measurment number")
 plt.ylabel("ervs")
