bd22c9097e9566a99d0cf56c338806a3403de50f
Thomaslund1 <159867737+Thomaslund1@users.noreply.github.com>
2024-07-29 14:17:52 -0500

    Update 3dPlots.py

--- examples/3dPlots.py
+++ examples/3dPlots.py
@@ -2,6 +2,9 @@ import h5py as hpy
 import numpy as np
 import matplotlib.pyplot as plt
 import ryanFunctions as lib
+from datetime import datetime
+from mpl_toolkits.mplot3d import Axes3D
+
 
 #collect all needed files
 waves1 = hpy.File('/data1/linefits/outputsEtalonNEID/consolidatedData_current/All_centroidWl.hdf5','r')['dat'][:]
@@ -17,63 +20,48 @@ waves2 = lib.interpolate(waves2)
 ords1 = lib.interpolate(ords1)
 ords2 = lib.interpolate(ords2)
 
-#convert wavelengths to velocities
+#convert wavelengths to velocities (comparing relative drift differences between fibers instead of absolute drifts)
+#to see absolute drift differences one might subtract wavelengths, then run that array through wl2vel with a ruler array of wavelengths from one of the initial wavelength arrays
 ervs1 = lib.wl2vel(waves1)
 ervs2 = lib.wl2vel(waves2)
 
+#bin the velocities
 ervs1 = lib.getVels(ervs1,ords1,5,0)
 ervs2 = lib.getVels(ervs2,ords2,5,0)
 
-from datetime import datetime
-
+#convert times to unix timestamps
 Times1 = []
 for i in range(len(times)):
     Times1.append((datetime.fromisoformat(str(times[i][:8] + str("T") + times[i][8:]))).timestamp())
-Times2 = []
-for i in range(len(times1)):
-    Times2.append((datetime.fromisoformat(str(times1[i][:8] + str("T") + times1[i][8:]))).timestamp())
 
+#subtract the binned velocities to  see how they differ
 diffs = np.subtract(ervs1,ervs2)
 
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import h5py as hpy
-%matplotlib widget
-
-
+#flatten the array and take a boxcar median for outliers
 newErvs1 = []
 for i in diffs:
     for j in i:
         newErvs1.append(lib.boxcar_median(j,50))
 newErvs1 = np.array(newErvs1)
 
+#duplicate the times array to match the dimensions of our data array
 newTimes = []
 for i in range(400):
     newTimes.append(Times1)
 newTimes = np.array(newTimes)
 newTimes.shape
 
+#fancy 3d plots
+y = np.arange(newErvs1.shape[0])  # create one axes for the length of our data array (these will be used for order bins in this case)
+x = Times1 # next axis is for time
+x, y = np.meshgrid(x, y) # turn to meshgrid for plotting
+fig = plt.figure(figsize=(14, 7)) # make a figure
+ax = fig.add_subplot(111, projection='3d') # matplotlib 3d schenangians 
+ax.plot_surface(x, y, newErvs1, cmap='viridis', edgecolor='none', rstride=8, cstride=8) # plot the x and y axes from earlier against our data array
 
-y = np.arange(newErvs1.shape[0])  # This will be [0, 1, 2, ..., 399]
-x = Times1  # Time array
-
-# Create meshgrid for x (time) and y (rows of the data)
-x, y = np.meshgrid(x, y)
-
-# Create the figure and 3D axes
-fig = plt.figure(figsize=(14, 7))
-ax = fig.add_subplot(111, projection='3d')
-
-# Plot the surface
-ax.plot_surface(x, y, newErvs1, cmap='viridis', edgecolor='none', rstride=8, cstride=8)
-
-# Set labels
+#formatting
 ax.set_xlabel('Time')
 ax.set_ylabel('Row Index')
 ax.set_zlabel('Value')
 ax.set_title('3D Plot of master file differences')
-
-# Show the plot
 plt.show()
